f273129da54f03bb9bcfbbe0a329c2459d361a48
KelliMK <kelli.m.knipe@gmail.com>
2018-10-28 19:13:37 -0400

    okay please this time

--- index_js.js
+++ index_js.js
@@ -1,32 +1,14 @@
-let slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-    showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-    let i;
-    let x = document.getElementsByClassName("mySlides");
-    if (n > x.length) {slideIndex = 1} 
-    if (n < 1) {slideIndex = x.length} ;
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none"; 
-    }
-    x[slideIndex-1].style.display = "block"; 
-}
-
-var coll = document.getElementByClassName("collapse-list");
+var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
 	coll[i].addEventListener("click", function() {
 		this.classList.toggle("active");
-		let content = this.nextElementSibling;
-		if (content.style.display === "block") {
-			content.style.display = "none";
+		var content = this.nextElementSibling;
+		if (content.style.maxHeight) {
+			content.style.maxHeight = null;
 		} else {
-			content.style.display = "block";
+			content.style.maxHeight = content.scrollHeight + "px";
 		}
 	});
 }
