ae5f7342ce119d2f98b52924baa0a36df55058b9
KelliMK <kelli.m.knipe@gmail.com>
2018-11-13 11:02:34 -0500

    separated index html from index js

---
+++ index.js
@@ -0,0 +1,120 @@
+
+<!-- Collapsible script -->
+const coll = document.getElementsByClassName("collapsible");
+let i = 0;
+
+for (i; i < coll.length; i++) {
+coll[i].addEventListener("click", function() {
+this.classList.toggle("activelist");
+let content = this.nextElementSibling;
+if (content.style.maxHeight) {
+content.style.maxHeight = null;
+} else {
+content.style.maxHeight = content.scrollHeight + "px";
+}
+});
+}
+
+function myFunction() {
+let x = document.getElementById("myTopnav");
+if (x.className === "topnav") {
+x.className += " responsive";
+} else {
+x.className = "topnav";
+}
+}
+
+<!-- Slideshow or Image Carosoul script -->
+
+let finishedSlideIndex = 1;
+let dropFinishedDescription = 1;
+showFinishedDivs(finishedSlideIndex);
+showFinishedDescription(dropFinishedDescription, finishedSlideIndex)
+
+function toggleFinishedDescription(n) {
+showFinishedDescription(dropFinishedDescription += n, finishedSlideIndex);
+}
+
+function plusFinishedDivs(n) {
+showFinishedDivs(finishedSlideIndex += n);
+showFinishedDescription(dropFinishedDescription, finishedSlideIndex);
+}
+
+function showFinishedDescription(x, n) {
+if (x % 4 == 4) {
+document.getElementById("finished-description").innerHTML = null;
+} else if (n % 4 == 1) {
+document.getElementById("finished-description").innerHTML = "a clock";
+} else if (n % 4 == 2) {
+document.getElementById("finished-description").innerHTML = "a drum kit";
+} else if (n % 4 == 3) {
+document.getElementById("finished-description").innerHTML = "a bunch of fuckers killing people for money";
+} else if (n % 4 == 0) {
+document.getElementById("finished-description").innerHTML = "my dream job";
+}
+}
+
+function showFinishedDivs(n) {
+let j;
+let y = document.getElementsByClassName("finished-slides");
+if (n > y.length) {finishedSlideIndex = 1} 
+if (n < 1) {finishedSlideIndex = y.length} ;
+for (j = 0; j < y.length; j++) {
+y[j].style.display = "none"; 
+}
+y[finishedSlideIndex-1].style.display = "block"; 
+}
+
+
+
+let currentSlideIndex = 1;
+let dropCurrentDescription = 1;
+showCurrentDivs(currentSlideIndex);
+showCurrentDescription(dropCurrentDescription, currentSlideIndex)
+
+function toggleCurrentDescription(n) {
+showCurrentDescription(dropCurrentDescription += n, finishedSlideIndex);
+}
+
+function plusCurrentDivs(n) {
+showCurrentDivs(currentSlideIndex += n);
+showCurrentDescription(dropCurrentDescription, currentSlideIndex);
+}
+
+function showCurrentDescription(x, n) {
+if (x % 2 == 2) {
+document.getElementById("current-description").innerHTML = null;
+} else if (n % 2 == 1) {
+document.getElementById("current-description").innerHTML = "I'm making a text adventure";
+} else if (n % 2 == 0) {
+document.getElementById("current-description").innerHTML = "I'm making a game where the player just kind of exists";
+}
+}
+
+function showCurrentDivs(n) {
+let k;
+let z = document.getElementsByClassName("current-slides");
+if (n > z.length) {currentSlideIndex = 1} 
+if (n < 1) {currentSlideIndex = z.length} ;
+for (k = 0; k < z.length; k++) {
+z[k].style.display = "none"; 
+}
+z[currentSlideIndex-1].style.display = "block"; 
+}
+
+const modal = document.getElementById('contact-me');
+const modalBtn = document.getElementById("contact-me-button");
+let modalSpan = document.getElementsByClassName("close")[0];
+modalBtn.onclick = function() {
+modal.style.display = "block";
+}
+
+modalSpan.onclick = function() {
+modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+if (event.target == modal) {
+modal.style.display = "none";
+}
+}
